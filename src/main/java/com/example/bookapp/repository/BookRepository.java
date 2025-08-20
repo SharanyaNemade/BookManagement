5c4b48b550ba3f9020c9c0e5bc66ecaec0f8ecec
Sharanya Nemade <87638228+SharanyaNemade@users.noreply.github.com>
2025-08-20 23:16:36 +0530

    Create BookRepository.java

---
+++ src/main/java/com/example/bookapp/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.example.bookapp.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.bookapp.entity.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByUserId(Long userId);
+}
